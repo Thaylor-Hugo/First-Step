bc3c8b6cf3de6ee956e68be1ae60613d18156272
Thaylor-Hugo <thaylorhugo@gmail.com>
2024-03-17 23:38:06 -0300

    🔥 Remove omf and bass install (for now)

--- tools/fish_install.sh
+++ tools/fish_install.sh
@@ -4,7 +4,7 @@
 install_fish() {
     apt-get install fish -y >> log/install.log
     chsh -s $(which fish) >> log/install.log
-    yes | curl -L https://get.oh-my.fish | fish >> log/install.log
-    omf install bass >> log/install.log
-    echo "bass source /opt/ros/iron/setup.bash" >> ~/.config/fish/config.fish
+    # yes | curl -L https://get.oh-my.fish | fish >> log/install.log
+    # omf install bass >> log/install.log
+    # echo "bass source /opt/ros/iron/setup.bash" >> ~/.config/fish/config.fish
 }
