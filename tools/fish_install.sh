1d8b7b9abd98f0870d47dde935993228340e3487
Thaylor-Hugo <thaylorhugo@gmail.com>
2024-03-18 23:20:08 -0300

    🔨 Create fish config dir

--- tools/fish_install.sh
+++ tools/fish_install.sh
@@ -31,7 +31,8 @@ install_fish() {
     sudo apt-get install fish -y >> log/install.log
     sudo chsh -s $(which fish) >> log/install.log
     config_fish $1 $2 $3 > config/config.fish
-    mv -f config/config.fish ~/.config/fish/
+    mkdir -p ~/.config/fish
+    cp -f config/config.fish ~/.config/fish/
     # yes | curl -L https://get.oh-my.fish | fish >> log/install.log
     # omf install bass >> log/install.log
 }
