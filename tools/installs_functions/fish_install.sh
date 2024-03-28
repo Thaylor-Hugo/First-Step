ab4b8a401a277bb17502e25e3191cdef4ee2b96a
Thaylor-Hugo <thaylorhugo@gmail.com>
2024-03-28 18:45:36 -0300

    💄 Gum on fish

--- tools/installs_functions/fish_install.sh
+++ tools/installs_functions/fish_install.sh
@@ -2,11 +2,12 @@
 
 # Install fish shell
 install_fish() {
-    sudo apt-get install fish -y >> log/install.log
-    sudo chsh -s $(which fish) >> log/install.log
+    local title="Installing 'Fish - Interactive shell'..."
+    local gum_spin='gum spin --spinner line --title'
+    $gum_spin "$title" -- sudo apt-get install fish -y >> log/install.log
+    $gum_spin "$title" -- sudo chsh -s $(which fish) >> log/install.log
     mkdir -p ~/.config/fish
     cp -f config/config.fish ~/.config/fish/
     # yes | curl -L https://get.oh-my.fish | fish >> log/install.log
     # omf install bass >> log/install.log
 }
-
