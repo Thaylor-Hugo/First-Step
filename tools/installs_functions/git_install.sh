fe584faa6ee3f6c1be1f660279f807ce8e4c8ee0
Thaylor-Hugo <thaylorhugo@gmail.com>
2024-03-28 18:02:08 -0300

    💄 Gum on git

--- tools/installs_functions/git_install.sh
+++ tools/installs_functions/git_install.sh
@@ -4,8 +4,8 @@ git_email=""
 git_name=""
 
 get_git_info() {
-    read -p "Enter your git usergit_name: " git_name
-    read -p "Enter your git git_email: " git_email
+    git_name=$(gum input --prompt.foreground "#0FF" --prompt "* Enter your git user_name: " --placeholder "your_user_name") 
+    git_email=$(gum input --prompt.foreground "#0FF" --prompt "* Enter your git user_email: " --placeholder "your_email@email.com")
 }
 
 configure_git() {
@@ -15,16 +15,22 @@ configure_git() {
 
 configure_ssh() {
     ssh-keygen -t ed25519 -C "$git_email" -f ~/.ssh/id_ed25519
-    eval "$(ssh-agent -s)" >> log/install.log
-    ssh-add ~/.ssh/id_ed25519 >> log/install.log
+    $gum_spin "$title" -- eval "$(ssh-agent -s)" >> log/install.log
+    $gum_spin "$title" -- ssh-add ~/.ssh/id_ed25519 >> log/install.log
     xclip -sel clip < ~/.ssh/id_ed25519.pub
     echo "The SSH key has been copied to the clipboard"
 }
 
 install_git() {
-    sudo apt-get install git -y >> log/install.log
-    get_git_info
-    echo "Configuring git..."
-    configure_git
-    configure_ssh
+    local title="Installing 'Git - Version control system'..."
+    local gum_spin='gum spin --spinner line --title'
+    $gum_spin "$title" -- sudo apt-get install git -y >> log/install.log
+    
+    local title="Configuring 'Git - Version control system'..."
+    gum confirm "Configure Git?"
+    if [ $? -eq 0 ]; then
+        get_git_info
+        configure_git
+        configure_ssh
+    fi
 }
