2730cb8e3b3a2aee60cf1551eaff208aa452942b
Thaylor-Hugo <thaylorhugo@gmail.com>
2024-03-28 20:29:37 -0300

    ✨ Feature Gnome Extension

--- first_step.sh
+++ first_step.sh
@@ -15,6 +15,7 @@ echo "Initiating Install and Config"
 # Source the install file
 source tools/install.sh
 source tools/utils.sh
+source tools/gnome_extension_install.sh
 
 # Define the checklist items
 programing_list=(   "Git - Version control system" \
@@ -33,6 +34,23 @@ useful_list=(   "Discord - Communication platform" \
                 "CopyQ - Clipboard manager" \
                 "Baobab - Disk usage analiser" \
                 "Charge Rules - Auto change power profile")
+
+extensions_list=(   "Battery Health Charging - Battery health and charging information" \
+                    "Blur My Shell - Blur the shell background and lock screen" \
+                    "Caffeine - Prevent the activation of the screensaver" \
+                    "Clipboard Indicator - Clipboard manager" \
+                    "Dash to Dock - Move the dash out of the overview transforming it in a dock" \
+                    "Extension List - Manage GNOME Shell extensions" \
+                    "GNOME 40 UI Improvements - GNOME 40 UI Improvements" \
+                    "GSConnect - KDE Connect implementation for GNOME" \
+                    "Impatience - Speed up the GNOME Shell animations" \
+                    "Net Speed Simplified - Display the network speed" \
+                    "Order Gnome Shell Extensions - Order Gnome Shell Extensions" \
+                    "Refresh WiFi Connections - Refresh WiFi Connections" \
+                    "Sound Output Device Chooser - Sound Output Device Chooser" \
+                    "User Themes - User Themes" \
+                    "Vitals - System monitoring" \
+                    "WinTile - Windows 10 window tiling for GNOME")
 
 # Function to display the checklist
 display_checklist() {
@@ -54,10 +72,21 @@ display_checklist() {
     local selected_useful_items=$(gum choose --no-limit "${useful_list[@]}")
     check_cancel
 
+    # Display the checklist for gnome extensions
+    clear
+    gum style \
+        --foreground 212 --border-foreground 212 --border double \
+        --align center --width 50 --margin "1 2" --padding "2 4" \
+        "Install and Configure Gnome Extensions" 'Choose the items you would like!'
+    local selected_extensions=$(gum choose --no-limit "${extensions_list[@]}")
+
+    # Separate the selected items
     local IFS=$'\n'
     selected_programing_items=($selected_programing_items)
     selected_useful_items=($selected_useful_items)
+    selected_extensions=($selected_extensions)
 
+    # Combine the selected items
     choices=("${selected_programing_items[@]}" "${selected_useful_items[@]}")
 }
 
@@ -80,6 +109,9 @@ main() {
     echo "System updated!"
     gum spin --spinner line --title "Upgrading system" -- sudo apt-get upgrade -y >> log/install.log
     echo "System upgraded!"
+
+    # Install gnome extensions
+    install_gnome_extensions "${selected_extensions[@]}"
 
     echo -e "\nInstallation Done! Please reboot to finish configuration"
     read -p "Reboot now? (y) or (n): "
