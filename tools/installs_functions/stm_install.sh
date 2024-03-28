ec802f53ed52ba756741d7b2970e5876f7052355
Thaylor-Hugo <thaylorhugo@gmail.com>
2024-03-28 02:00:10 -0300

    ✨ Gum to STM32Cube software

--- tools/installs_functions/stm_install.sh
+++ tools/installs_functions/stm_install.sh
@@ -2,29 +2,36 @@
 
 unzip_cube() {
     for cube_prog in stm/en.stm32cube*.zip; do
-        unzip $cube_prog -d ${cube_prog%".zip"}
+        $gum_spin "$title" -- unzip $cube_prog -d ${cube_prog%".zip"}
         rm -r $cube_prog
     done
 }
 
 install_cube_prog() {
-    sudo ./stm/en.stm32cubeprg*/SetupSTM32CubeProgrammer*.linux >> log/install.log
-    sudo apt-get install libusb-1.0-0-dev -y >> log/install.log
-    sudo cp /usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/Drivers/rules/*.* /etc/udev/rules.d
+    local title="Installing 'STM32CubeProgrammer'..."
+    $gum_spin "$title" -- sudo ./stm/en.stm32cubeprg*/SetupSTM32CubeProgrammer*.linux >> log/install.log
+    $gum_spin "$title" -- sudo apt-get install libusb-1.0-0-dev -y >> log/install.log
+    $gum_spin "$title" -- sudo cp /usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/Drivers/rules/*.* /etc/udev/rules.d
     echo "export PATH=\$PATH:/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin" >> ~/.bashrc
+    rm -r ./stm/en.stm32cubeprg*
 }
 
 install_cube_mx() {
-    sudo ./stm/en.stm32cubemx*/SetupSTM32CubeMX* >> log/install.log
+    local title="Installing 'STM32CubeMX'..."
+    $gum_spin "$title" -- sudo ./stm/en.stm32cubemx*/SetupSTM32CubeMX* >> log/install.log
     echo "export CUBE_PATH=/usr/local/STMicroelectronics/STM32Cube/STM32CubeMX/" >> ~/.bashrc
-    echo -e "cubemx() {\n    \$CUBE_PATH/STM32CubeMX \$1\n}" >> ~/.bashrc
+    echo -e "cubemx() {\n    \$CUBE_PATH/STM32CubeMX \$1 &> /dev/null &\n}" >> ~/.bashrc
+    rm -r ./stm/en.stm32cubemx*
 }
 
 install_cube_mon() {
+    local title="Installing 'STM32Monitor'..."
     echo "CubeMonitor not supported yet"
 }
 
 install_cube() {
+    local title="Installing 'STM32Cube - CubeProgrammer, CubeMX and CubeMonitor'..."
+    local gum_spin='gum spin --spinner line --title'
     unzip_cube
     install_cube_prog
     install_cube_mx
